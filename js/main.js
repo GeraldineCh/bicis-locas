f7d9ba6407617774dc98ae898d8a8df3d3597e0a
Geraldine Chauca <gchaucae@gmail.com>
2017-03-03 17:35:26 -0500

    Prueba exitosa de html y .js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-function validate(){
+function validateForm(){
  var nombre = document.getElementById("name").value;
  var apellido = document.getElementById("lastname").value;
  var contrasena = document.getElementById("input-password").value;
  var correo = document.getElementById("input-email").value;
- var modelo = document.getElementById("opciones").value;
+ var modelo = document.getElementsByClassName("form-control").value;
 
 if((nombre != "" && apellido  != "") && (contrasena != "" && modelo != "")){
-  if(!name.match(/[A-Z]{1,}[a-z]+/)){
+  if(!nombre.match(/[A-Z]{1,}[a-z]+/)){
     alert("Estructura de nombre no válida: \n Primera letra debe ser máyuscula");
   }
   if(!apellido.match(/[A-Z]{1,}[a-z]+/)){
@@ -16,17 +16,12 @@ if((nombre != "" && apellido  != "") && (contrasena != "" && modelo != "")){
     alert("Estructura de correo no válida: \n Ingresar email@domain.com");
   }
 
-  if(contrasena.length != 6 || contrasena == "password" || contrasena == "123456" || password == "098754"){
+  if(contrasena.length != 6 || contrasena == "password" || contrasena == "123456" || contrasena == "098754"){
     alert("Estructura de contraseña no válida: \n Ingresar contraseña de 6 caracteres");
   }
 
 }else{
-  alert("Falta ingresar iformación obligatoria");
+  alert("Falta ingresar iformación obligatoria.");
 }
 
-document.getElementById("name").value = "";
-document.getElementById("lastname").value = "";
-document.getElementById("input-password").value = "";
-document.getElementById("input-email").value = "";
-document.getElementById("opciones").value = "";
 }
