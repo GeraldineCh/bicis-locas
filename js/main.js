f5b7914aa8eacfd637aa9df7bb90ed59376e4b29
Geraldine Chauca <gchaucae@gmail.com>
2017-03-03 17:53:04 -0500

    Cambios de formato

--- js/main.js
+++ js/main.js
@@ -9,9 +9,11 @@ if((nombre != "" && apellido  != "") && (contrasena != "" && modelo != "")){
   if(!nombre.match(/[A-Z]{1,}[a-z]+/)){
     alert("Estructura de nombre no válida: \n Primera letra debe ser máyuscula");
   }
+
   if(!apellido.match(/[A-Z]{1,}[a-z]+/)){
     alert("Estructura de apellido no válida: \n Primera letra debe ser máyuscula");
   }
+
   if(!correo.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.com$/)){
     alert("Estructura de correo no válida: \n Ingresar email@domain.com");
   }
@@ -20,8 +22,8 @@ if((nombre != "" && apellido  != "") && (contrasena != "" && modelo != "")){
     alert("Estructura de contraseña no válida: \n Ingresar contraseña de 6 caracteres");
   }
 
-}else{
-  alert("Falta ingresar iformación obligatoria.");
 }
+else
+	{alert("Falta ingresar iformación obligatoria.");}
 
 }
